3666cb72592f7b7395dcf984915afe7a091951d1
Sorongos <dosora9@gmail.com>
2024-04-08 12:58:12 -0700

    시간 포맷 추가, 앱바에 탭 인덱스 추가

--- lib/main.dart
+++ lib/main.dart
@@ -34,45 +34,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  // 변경된 부분
-  late IOWebSocketChannel channel;
-  late TabController _tabController;
-  List<String> macAddresses = [];
-  Map<String, DeviceData> deviceDataMap = {};
-  double _progress = 0.0; // 프로그레스 바의 진행 상태를 저장할 변수 추가
+  late IOWebSocketChannel channel; // 웹소켓 채널
+  late TabController _tabController; // 탭 컨트롤러
+  List<String> macAddresses = []; // 탭 제목을 표시하기 위한 macaddr만을 저장하는 변수
+  Map<String, DeviceData> deviceDataMap = {}; // <macaddr, 서버에서 받아온 정보들>
+  int _currentTabIndex = 0; // 현재 선택된 탭의 인덱스를 추적하는 변수
 
   @override
   void initState() {
     super.initState();
-    channel = IOWebSocketChannel.connect('ws://192.168.1.105:8080');
+    channel = IOWebSocketChannel.connect('ws://192.168.1.102:8080'); // 웹소켓 채널
     channel.stream.listen(_onMessage);
     _tabController = TabController(length: 0, vsync: this);
-    _tabController.addListener(_updateProgress); // TabController에 리스너 추가
-  }
-
-  void _updateProgress() {
-    // 현재 탭의 인덱스를 기반으로 프로그레스 바의 값을 업데이트하는 함수
-    print("rssi: _tabController.length ${_tabController.length}");
-    if (_tabController.length > 1) {
-      setState(() {
-        _progress = _tabController.index / (_tabController.length - 1);
-      });
-      print("rssi: progress $_progress");
-    }
+    _tabController.addListener(() {
+      // 탭 컨트롤러의 리스너 추가
+      if (!_tabController.indexIsChanging) {
+        setState(() {
+          _currentTabIndex = _tabController.index; // 현재 선택된 탭의 인덱스 업데이트
+        });
+      }
+    });
   }
 
   void _onMessage(dynamic message) {
+    // 메시지를 JSON 형태로 디코딩
     final jsonResponse = json.decode(message);
 
+    // 메시지 내용 출력
     print("rssi: json msg $message");
 
+    // JSON 응답이 유효하고 'macaddr' 키가 존재하는지 확인
     if (jsonResponse != null && jsonResponse['macaddr'] != null) {
+      // jsonResponse에서 필요한 데이터 추출, 값이 없으면 0 또는 빈 문자열로 대체
       String macaddr = jsonResponse['macaddr'];
-      int rssival = jsonResponse['rssival'] ?? 'N/A';
-      double kalmanval = jsonResponse['kalmanval'].toDouble() ?? 'N/A';
-      String measuretime = jsonResponse['measuretime'] ?? 'N/A';
-      int scancnt = jsonResponse['scancnt'] ?? 'N/A';
+      int rssival = jsonResponse['rssival'] ?? 0;
+      double kalmanval = jsonResponse['kalmanval'].toDouble() ?? 0.0;
+      String measuretime = jsonResponse['measuretime'] ?? '';
+      int scancnt = jsonResponse['scancnt'] ?? 0;
 
+      // DeviceData 객체 생성
       DeviceData deviceData = DeviceData(
         macaddr: macaddr,
         rssival: rssival,
@@ -81,18 +81,55 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
         scancnt: scancnt,
       );
 
+      // macAddresses 리스트에 macaddr이 없는 경우 새로 추가
       if (!macAddresses.contains(macaddr)) {
         setState(() {
           macAddresses.add(macaddr);
-          _tabController.dispose();
-          _tabController =
-              TabController(length: macAddresses.length, vsync: this);
+          int previousIndex = _tabController.index; // 현재 인덱스를 저장
+          _tabController.dispose(); // 기존의 탭 컨트롤러를 해제
+
+          // 새로운 탭 컨트롤러를 생성하고 이전에 선택된 탭 인덱스를 초기 인덱스로 설정
+          _tabController = TabController(
+              length: macAddresses.length,
+              vsync: this,
+              initialIndex: previousIndex); // 이전 인덱스를 초기 인덱스로 설정
+
+          // 탭 컨트롤러의 리스너를 추가하여 탭 변경 시 이벤트를 처리
+          _tabController.addListener(() {
+            if (!_tabController.indexIsChanging) {
+              setState(() {
+                _currentTabIndex = _tabController.index; // 현재 선택된 탭의 인덱스 업데이트
+              });
+            }
+          });
         });
       }
 
+      // deviceDataMap에 macaddr을 키로 하여 deviceData를 저장
       setState(() {
         deviceDataMap[macaddr] = deviceData;
       });
+    }
+  }
+
+  /// '20240327085015' -> '2024년 3월 27일 8시 50분 15초'로 포맷을 변경해주는 함수
+  String formatMeasureTime(String measuretime) {
+    try {
+      // 입력된 문자열에서 연, 월, 일을 나타내는 부분과 시, 분, 초를 나타내는 부분 사이에 'T' 문자를 삽입
+      // ISO 8601 날짜 및 시간 형식을 따르기 위함
+      // "20240408123045" -> "20240408T123045"
+      String dateWithT =
+          measuretime.substring(0, 8) + 'T' + measuretime.substring(8);
+
+      // DateTime 객체 생성
+      DateTime dateTime = DateTime.parse(dateWithT);
+
+      // 문자열 변환
+      return DateFormat('yyyy년 M월 d일 H시 m분 s초').format(dateTime);
+    } catch (e) {
+      // 파싱 과정에서 오류가 발생하면 빈 문자열을 반환하고, 에러를 출력함
+      print("Error parsing measuretime: $e");
+      return '';
     }
   }
 
@@ -101,21 +138,18 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(
+            '${widget.title} (${_currentTabIndex + 1}/${_tabController.length})'), // 현재 선택된 탭 정보 표시
         bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(50.0), // TabBar와 프로그레스 바를 포함할 공간 확보
+          preferredSize: const Size.fromHeight(50.0),
+          // TabBar와 프로그레스 바를 포함할 공간 확보
           child: Column(
             children: [
-              LinearProgressIndicator(
-                value: _progress, // 프로그레스 바의 값
-                minHeight: 4.0, // 프로그레스 바의 높이 설정
-                backgroundColor: Colors.grey, // 프로그레스 바의 배경색 설정
-                valueColor: AlwaysStoppedAnimation<Color>(Colors.blue), // 프로그레스 바의 색상 설정
-              ),
               TabBar(
                 controller: _tabController,
                 isScrollable: true,
-                tabs: macAddresses.map((macaddr) => Tab(text: macaddr)).toList(),
+                tabs:
+                    macAddresses.map((macaddr) => Tab(text: macaddr)).toList(),
               ),
             ],
           ),
@@ -128,12 +162,23 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
           return Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start, // 좌측 정렬
               children: [
-                Text('Mac Address: ${data?.macaddr ?? ''}'), // string
-                Text('RSSI Value: ${data?.rssival ?? 0}'), // int
-                Text('Kalman Value: ${data?.kalmanval ?? 0.0}'), // double
-                Text('Measure Time: ${data?.measuretime ?? ''}'), // string
-                Text('Scan Count: ${data?.scancnt ?? 0}'), // int
+                Text('Mac Address: ${data?.macaddr ?? ''}',
+                    style: TextStyle(fontSize: 16.0)), // 텍스트 사이즈 조정
+                SizedBox(height: 8.0),
+                Text('RSSI Value: ${data?.rssival ?? 0}',
+                    style: TextStyle(fontSize: 16.0)), // 텍스트 사이즈 조정
+                SizedBox(height: 8.0),
+                Text('Kalman Value: ${data?.kalmanval ?? 0.0}',
+                    style: TextStyle(fontSize: 16.0)), // 텍스트 사이즈 조정
+                SizedBox(height: 8.0),
+                Text(
+                    'Measure Time: ${formatMeasureTime(data?.measuretime ?? '')}',
+                    style: TextStyle(fontSize: 16.0)), // 텍스트 사이즈 조정
+                SizedBox(height: 8.0),
+                Text('Scan Count: ${data?.scancnt ?? 0}',
+                    style: TextStyle(fontSize: 16.0)), // 텍스트 사이즈 조정
               ],
             ),
           );
@@ -145,7 +190,6 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   @override
   void dispose() {
     channel.sink.close();
-    _tabController.removeListener(_updateProgress); // 리스너 제거
     _tabController.dispose();
     super.dispose();
   }
